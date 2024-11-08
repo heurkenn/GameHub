388ff49e24c0480e94a8ada7fa5fe7727bebe096
jjotogo <165060983+jjotogo@users.noreply.github.com>
2024-11-08 11:28:07 +0000

    Création de la page d'inscription

--- gamehub/src/main/java/fr/gamehub/gamehub/controller/PageController.java
+++ gamehub/src/main/java/fr/gamehub/gamehub/controller/PageController.java
@@ -4,10 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class IndexController {
+public class PageController {
 
     @GetMapping("/")
     public String index() {
         return "index"; // Retourne la vue 'index.html' qui se trouve dans le dossier 'templates'
     }
+    
+    @GetMapping("/inscription")
+    public String inscription(){
+        return "inscription";
+    }
+    
 }
