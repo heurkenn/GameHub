500c8e46574453cf083e6664c065ca2689a64a60
jjotogo <165060983+jjotogo@users.noreply.github.com>
2024-11-15 11:47:31 +0000

    Réparation de toutes les merde qui ne fonctionne pas + reparation port 8080

--- gamehub/src/main/resources/schema.sql
+++ gamehub/src/main/resources/schema.sql
@@ -10,10 +10,7 @@ CREATE TABLE app_user (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    username VARCHAR(50) NOT NULL,
-    name VARCHAR(50) NOT NULL,
-    surnamme VARCHAR(50) NOT NULL,
-    birthDate DATE NOT NULL
+    username VARCHAR(50) NOT NULL
 );
 
 -- Créer la table game
