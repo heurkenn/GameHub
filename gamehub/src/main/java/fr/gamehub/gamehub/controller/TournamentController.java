b48d77b9756acdf77ce319e3dd2a84c772b675a9
pikmin53 <165147110+pikmin53@users.noreply.github.com>
2024-11-15 10:09:46 +0000

    tf

---
+++ gamehub/src/main/java/fr/gamehub/gamehub/controller/TournamentController.java
@@ -0,0 +1,13 @@
+package fr.gamehub.gamehub.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import fr.gamehub.gamehub.repository.*;
+@Controller
+public class TournamentController {
+
+
+    @Autowired
+    TournamentRepository tournamentrepository;
+}
--- gamehub/src/main/java/fr/gamehub/gamehub/model/Fight.java
+++ gamehub/src/main/java/fr/gamehub/gamehub/model/Fight.java
@@ -1,5 +1,29 @@
 package fr.gamehub.gamehub.model;
 
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "Tournament") 
+@Getter
+@Setter
+@NoArgsConstructor
 public class Fight {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @ManyToOne
+    @JoinColumn(name="tournament_id", nullable=false)
+    private Tournament tournoi;
+
+
+    private User joueur1;
+    private User joueur2;
+
+
 
 }
