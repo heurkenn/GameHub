a57a9a00399348107c0cb30c7b7683f22d4e7db9
Aure Nolhan <aurenolhan@cy-tech.fr>
2024-11-29 20:46:32 +0100

    css générale

--- gamehub/src/main/java/fr/gamehub/gamehub/controller/IndexController.java
+++ gamehub/src/main/java/fr/gamehub/gamehub/controller/IndexController.java
@@ -21,7 +21,7 @@ public class IndexController {
     @GetMapping("/")
     public String index(Model model) {
         // Ajouter trois jeux aléatoires au modèle
-        model.addAttribute("randomGames", gameService.findRandomGames(3));
+        model.addAttribute("randomGames", gameService.findRandomGames(6));
         return "index"; // Retourne la vue 'index.html' située dans 'src/main/resources/templates'
     }
 
