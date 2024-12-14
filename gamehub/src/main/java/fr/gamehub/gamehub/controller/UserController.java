115da2938ef2a53e6e1d190d154e4b673837d940
heurkenn <heurkenn@gmail.com>
2024-12-14 22:09:52 +0100

    merge effectué

--- gamehub/src/main/java/fr/gamehub/gamehub/controller/UserController.java
+++ gamehub/src/main/java/fr/gamehub/gamehub/controller/UserController.java
@@ -1,18 +1,17 @@
 package fr.gamehub.gamehub.controller;
 
-import jakarta.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import fr.gamehub.gamehub.model.*;
-import fr.gamehub.gamehub.repository.UserRepository;;
+
+import fr.gamehub.gamehub.model.User;
+import fr.gamehub.gamehub.repository.UserRepository;
+import jakarta.validation.Valid;;
 
 @Controller
 public class UserController {
