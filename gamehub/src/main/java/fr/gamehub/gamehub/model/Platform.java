174a7b70b5d1fd1b5222104c293935fcb68c48f4
heurkenn <heurkenn@gmail.com>
2024-12-14 13:10:33 +0100

    ok

--- gamehub/src/main/java/fr/gamehub/gamehub/model/Platform.java
+++ gamehub/src/main/java/fr/gamehub/gamehub/model/Platform.java
@@ -1,11 +1,14 @@
 package fr.gamehub.gamehub.model;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.PastOrPresent;
 import lombok.Getter;
@@ -30,6 +33,9 @@ public class Platform {
 
     @NotBlank(message = "Le studio de développement ne peut pas être vide")
     private String developerStudio;
+
+    @ManyToMany(mappedBy = "platforms")
+    private Set<Game> games = new HashSet<>();
 
     // Lombok génère automatiquement les getters, setters, et le constructeur sans argument
 }
